37ad203e97821b9a0afcf42cd73300e2add9685e
Vladimir V.M <vladimir.myagdeev@noveogroup.com>
2023-12-20 06:53:24 +0400

    feat<config>: default DURATION set to 5

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports.SEGMENT_DURATION =
-  (process.env.DURATION && parseInt(process.env.DURATION)) || 3;
+  (process.env.DURATION && parseInt(process.env.DURATION)) || 5;
 module.exports.API_KEY = process.env.OPENAI_API_KEY;
